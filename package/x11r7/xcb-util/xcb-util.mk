1bb15a4a4f22bb11f825dc7ac891c4bf60ef799a
John Voltz <john.voltz@gmail.com>
2009-02-10 15:13:07 +0000

    version bump xcb-util

--- package/x11r7/xcb-util/xcb-util.mk
+++ package/x11r7/xcb-util/xcb-util.mk
@@ -3,7 +3,7 @@
 # xcb-util
 #
 #############################################################
-XCB_UTIL_VERSION = 0.2
+XCB_UTIL_VERSION = 0.3.3
 XCB_UTIL_SOURCE = xcb-util-$(XCB_UTIL_VERSION).tar.bz2
 XCB_UTIL_SITE = http://xcb.freedesktop.org/dist/
 
