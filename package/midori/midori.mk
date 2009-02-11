a597e98c0c4fed77cf33019a387f5ae879945639
John Voltz <john.voltz@gmail.com>
2009-02-11 18:24:17 +0000

    update midori for the stupid WAF build system

--- package/midori/midori.mk
+++ package/midori/midori.mk
@@ -4,13 +4,59 @@
 #
 #############################################################
 
-MIDORI_VERSION = 0.0.18
-MIDORI_SOURCE = midori-$(MIDORI_VERSION).tar.gz
-MIDORI_SITE = http://software.twotoasts.de/media/midori/
-MIDORI_AUTORECONF = YES
-MIDORI_INSTALL_STAGING = NO
-MIDORI_INSTALL_TARGET = YES
+MIDORI_VERSION = 0.1.2
+MIDORI_SOURCE = midori-$(MIDORI_VERSION).tar.bz2
+MIDORI_SITE = http://goodies.xfce.org/releases/midori/
+MIDORI_DIR:=$(BUILD_DIR)/midori-$(MIDORI_VERSION)
 
-MIDORI_DEPENDENCIES = uclibc pkgconfig webkit libsexy $(XSERVER)
+$(DL_DIR)/$(MIDORI_SOURCE):
+	 $(call DOWNLOAD,$(MIDORI_SITE),$(MIDORI_SOURCE))
 
-$(eval $(call AUTOTARGETS,package,midori))
+midori-source: $(DL_DIR)/$(MIDORI_SOURCE)
+
+$(MIDORI_DIR)/.unpacked: $(DL_DIR)/$(MIDORI_SOURCE)
+	$(BZCAT) $(DL_DIR)/$(MIDORI_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	$(CONFIG_UPDATE) $(MIDORI_DIR)
+	toolchain/patch-kernel.sh $(MIDORI_DIR) package/midori/ \*.patch
+	touch $@
+
+$(MIDORI_DIR)/.configured: $(MIDORI_DIR)/.unpacked
+	(cd $(MIDORI_DIR); \
+		$(TARGET_CONFIGURE_OPTS) \
+		$(TARGET_CONFIGURE_ARGS) \
+		LDFLAGS="-L$(STAGING_DIR)/usr/avr32-linux-uclibc/lib -lstdc++" CXXFLAGS="-mrelax" CFLAGS="-mrelax" \
+		./configure \
+		--target=$(GNU_TARGET_NAME) \
+		--prefix=/usr \
+		--enable-sqlite --enable-addons \
+	);
+	touch $@
+
+$(MIDORI_DIR)/.cross_compile_fix: $(MIDORI_DIR)/.configured
+	$(SED) "s,^GLIB_GENMARSHAL.*,GLIB_GENMARSHAL = '/usr/bin/glib-genmarshal',g" $(MIDORI_DIR)/_build_/c4che/default.cache.py
+	$(SED) "s,^GLIB_MKENUMS.*,GLIB_MKENUMS = '/usr/bin/glib-mkenums',g" $(MIDORI_DIR)/_build_/c4che/default.cache.py
+	$(SED) "s,^LIB_WEBKIT = \[,LIB_WEBKIT = \['stdc++'\, ,g" $(MIDORI_DIR)/_build_/c4che/default.cache.py
+	touch $@
+
+$(MIDORI_DIR)/.done: $(MIDORI_DIR)/.cross_compile_fix
+	$(MAKE) -C $(MIDORI_DIR)
+	$(MAKE) -C $(MIDORI_DIR) install DESTDIR=$(TARGET_DIR)
+	touch $@
+
+midori: uclibc webkit libsexy $(XSERVER) $(MIDORI_DIR)/.done
+
+midori-clean:
+	rm -f $(TARGET_DIR)/bin/midori
+	-$(MAKE) -C $(MIDORI_DIR) clean
+
+midori-dirclean:
+	rm -rf $(MIDORI_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(BR2_PACKAGE_MIDORI),y)
+TARGETS+=midori
+endif
--- package/webkit/webkit.mk
+++ package/webkit/webkit.mk
@@ -3,17 +3,55 @@
 # webkit
 #
 #############################################################
-WEBKIT_VERSION = r31321
+
+WEBKIT_VERSION = r40813
 WEBKIT_SOURCE = WebKit-$(WEBKIT_VERSION).tar.bz2
-WEBKIT_SITE = http://nightly.webkit.org/files/trunk/src/
+WEBKIT_SITE = http://builds.nightly.webkit.org/files/trunk/src
+WEBKIT_DIR:=$(BUILD_DIR)/WebKit-$(WEBKIT_VERSION)
 
-WEBKIT_INSTALL_STAGING = YES
-WEBKIT_INSTALL_TARGET = YES
-WEBKIT_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) install
+$(DL_DIR)/$(WEBKIT_SOURCE):
+	 $(call DOWNLOAD,$(WEBKIT_SITE),$(WEBKIT_SOURCE))
 
-WEBKIT_AUTORECONF = YES
-WEBKIT_DEPENDENCIES = icu curl libxml2 libxslt libgtk2 sqlite
-WEBKIT_MAKE_OPT = GLIB_GENMARSHAL=$(HOST_GLIB)/bin/glib-genmarshal GLIB_MKENUMS=$(HOST_GLIB)/bin/glib-mkenums
+webkit-source: $(DL_DIR)/$(WEBKIT_SOURCE)
 
-$(eval $(call AUTOTARGETS,package,webkit))
+$(WEBKIT_DIR)/.unpacked: $(DL_DIR)/$(WEBKIT_SOURCE)
+	$(BZCAT) $(DL_DIR)/$(WEBKIT_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	$(CONFIG_UPDATE) $(WEBKIT_DIR)
+	toolchain/patch-kernel.sh $(WEBKIT_DIR) package/webkit/ \*.patch
+	touch $@
 
+$(WEBKIT_DIR)/.configured: $(WEBKIT_DIR)/.unpacked
+	(cd $(WEBKIT_DIR); ./autogen.sh; \
+		$(TARGET_CONFIGURE_OPTS) \
+		$(TARGET_CONFIGURE_ARGS) \
+		./configure \
+		--target=$(GNU_TARGET_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--build=$(GNU_HOST_NAME) \
+		--prefix=/usr \
+	);
+	touch $@
+
+$(WEBKIT_DIR)/.done: $(WEBKIT_DIR)/.configured
+	$(MAKE) -C $(WEBKIT_DIR) GLIB_GENMARSHAL=$(HOST_GLIB)/bin/glib-genmarshal \
+		GLIB_MKENUMS=$(HOST_GLIB)/bin/glib-mkenums
+	$(MAKE) -C $(WEBKIT_DIR) install DESTDIR=$(TARGET_DIR)
+	touch $@
+
+webkit: icu curl libxml2 libxslt libgtk2 sqlite $(WEBKIT_DIR)/.done
+
+webkit-clean:
+	rm -f $(TARGET_DIR)/bin/webkit
+	-$(MAKE) -C $(WEBKIT_DIR) clean
+
+webkit-dirclean:
+	rm -rf $(WEBKIT_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(BR2_PACKAGE_WEBKIT),y)
+TARGETS+=webkit
+endif
