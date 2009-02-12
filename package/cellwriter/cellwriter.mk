06387ad8ee38a5ea83eb645e86d3537e72166d6d
John Voltz <john.voltz@gmail.com>
2009-02-12 13:32:02 +0000

    add cellwriter handwriting recognition

---
+++ package/cellwriter/cellwriter.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# cellwriter
+#
+#############################################################
+CELLWRITER_VERSION = 1.3.4
+CELLWRITER_SOURCE = cellwriter-$(CELLWRITER_VERSION).tar.gz
+CELLWRITER_SITE = http://pub.risujin.org/cellwriter
+CELLWRITER_AUTORECONF = NO
+CELLWRITER_INSTALL_STAGING = NO
+CELLWRITER_INSTALL_TARGET = YES
+
+CELLWRITER_CONF_OPT = 
+
+CELLWRITER_DEPENDENCIES = uclibc 
+
+$(eval $(call AUTOTARGETS,package,cellwriter))
+
