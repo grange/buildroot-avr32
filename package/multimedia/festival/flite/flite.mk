d2c1a3427e0fe9163f007714445c585b363558c3
John Voltz <john.voltz@gmail.com>
2009-02-12 13:08:58 +0000

    fix flite build

--- package/multimedia/festival/flite/flite.mk
+++ package/multimedia/festival/flite/flite.mk
@@ -9,7 +9,10 @@ FLITE_SITE = http://www.speech.cs.cmu.edu/flite/packed/flite-$(FLITE_VERSION)
 FLITE_AUTORECONF = NO
 FLITE_INSTALL_STAGING = NO
 FLITE_INSTALL_TARGET = YES
-FLITE_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) STRIP=$(TARGET_STRIP) install
+FLITE_INSTALL_TARGET_OPT = INSTALLBINDIR=$(TARGET_DIR)/usr/bin \
+							INSTALLLIBDIR=$(TARGET_DIR)/usr/share/festival/lib \
+							INSTALLINCDIR=$(TARGET_DIR)/usr/include/flite \
+							STRIP=$(TARGET_STRIP) install
 
 FLITE_CONF_OPT = --prefix=/usr --libdir=/usr/share/festival/lib
 
