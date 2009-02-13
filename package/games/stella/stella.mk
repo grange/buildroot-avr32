dbd33d3a716b92c40d24dffc4ad0017a7c386630
John Voltz <john.voltz@gmail.com>
2009-02-13 20:23:49 +0000

    fix stella

--- package/games/stella/stella.mk
+++ package/games/stella/stella.mk
@@ -4,15 +4,19 @@
 #
 #############################################################
 STELLA_VERSION = 2.7.3a
-STELLA_SOURCE = stella-$(STELLA_VERSION).tar.gz
+STELLA_SOURCE = stella-$(STELLA_VERSION)-src.tar.gz
 STELLA_SITE = http://superb-west.dl.sourceforge.net/sourceforge/stella
 STELLA_AUTORECONF = NO
 STELLA_INSTALL_STAGING = NO
 STELLA_INSTALL_TARGET = YES
 
-STELLA_CONF_OPT = 
+STELLA_CONF_OPT = --with-zlib-prefix=$(STAGING_DIR)/usr --with-zlib-prefix=$(STAGING_DIR)/usr \
+					--prefix=/usr --bindir=/usr/bin --docdir=/usr/share/doc/stella --datadir=/usr/share 
+					
+
+STELLA_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
 STELLA_DEPENDENCIES = uclibc 
 
-$(eval $(call AUTOTARGETS,package,stella))
+$(eval $(call AUTOTARGETS,package/games,stella))
 
